0056dc8530a89187b01d2bf114ddb54c47b7c255
Panupat.Dev <123447221+BugNoTensei@users.noreply.github.com>
2025-05-02 17:01:30 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-const Selfintroduction = ({ name = "unknown", date0fbirth, hobbies }) => {
+const Selfintroduction = ({ name = "unknown", date0fbirth, hobbies = [] }) => {
   return (
     <div>
       <h1>"My name is {name}!"</h1>
       {date0fbirth ? <h2>I was born in {date0fbirth}</h2> : null}
-      {hobbies.join("")}
+      {hobbies.length > 0 ? (
+        <h2>My hobbies are: {hobbies.join(", ")}</h2>
+      ) : null}
     </div>
   );
 };
 
 function App() {
   return (
-    <div className='App'>
-      <Selfintroduction name="Kirito" date0fbirth="12 june 1970" hobbies={["Playgame", "Watching TV", "Eat"]} />
+    <div className="App">
+      <Selfintroduction
+        name="Test"
+        date0fbirth="12 june 1970"
+        hobbies={["play game0", "Eat"]}
+      />
     </div>
   );
 }
-export default App
+export default App;
